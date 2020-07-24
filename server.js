07d6263f26b834d749cc0cdb27aff76c05dbd1e6
Jacob Zirbel <jacobzirbel@gmail.com>
2020-07-24 14:17:50 -0500

    db

--- server.js
+++ server.js
@@ -2,12 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
-const cors = require("cors");
 const PORT = process.env.PORT || 3001;
-corsOptions = {
-  origin: "https://infinite-atoll-46092.herokuapp.com/",
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-};
+
 app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,7 +12,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 app.use(routes);
-mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/booksearch", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/booksearch", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
