45899a8454751baa151a94d8486c4582e7b1e6de
Jacob Zirbel <jacobzirbel@gmail.com>
2020-07-24 14:10:55 -0500

    cors

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
+const cors = require("cors");
 const PORT = process.env.PORT || 3001;
 corsOptions = {
   origin: "https://infinite-atoll-46092.herokuapp.com/",
