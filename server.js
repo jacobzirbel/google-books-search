9648a4ee0719cba5383371a2594bcf7e560a57f1
Jacob Zirbel <jacobzirbel@gmail.com>
2020-07-24 14:33:24 -0500

    heroku

--- server.js
+++ server.js
@@ -4,18 +4,17 @@ const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-app.use(routes);
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/booksearch", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`API Server on ${PORT}`);
